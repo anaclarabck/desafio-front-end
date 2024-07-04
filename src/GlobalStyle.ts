1d0ccd8680221f0db39bb1f6a79c60dd4f2c1908
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-04 10:00:33 -0300

    style: add background color and set margin and padding to zero

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import { colors } from "./constants";
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: "Roboto", sans-serif;
+    background-color: ${colors.grayLight};
+    margin: 0;
+    padding: 0;
   }
 `;
 
