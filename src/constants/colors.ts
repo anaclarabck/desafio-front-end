eec54613cd4a125a198fea26bb73bf87efa149c2
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-07 20:54:45 -0300

    chore: add black neutral to colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,6 +2,7 @@ const colors = {
   blueDark: "#5A84C0",
   blueLight: "#594Ed2",
   black: "#000000",
+  blackNeutral: "#1C1C1C",
   grayDark: "#9E9E9E",
   grayMedium: "#DFDFDF",
   grayLight: "#F0F0F0",
