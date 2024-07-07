92abe6fdd3bdaf0ad40de8710452b8db15067a18
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-07 18:22:14 -0300

    feat: add redux provider store to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyle from "./GlobalStyle.ts";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
