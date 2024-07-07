65aeb03b4f817737c3aa8a103c5fcc452ad669c1
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-07 18:38:06 -0300

    chore: add useWindowDimensions

--- src/hooks/useWindowDimensions/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions/useWindowDimensions.ts
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-interface WindowDimensions {
+type WindowDimensions = {
   width: number;
   height: number;
-}
+};
 
 function getWindowDimensions(): WindowDimensions {
   const { innerWidth: width, innerHeight: height } = window;
@@ -18,14 +18,14 @@ function useWindowDimensions(): WindowDimensions {
     getWindowDimensions()
   );
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+  const handleResize = useCallback(() => {
+    setWindowDimensions(getWindowDimensions());
+  }, []);
 
+  useEffect(() => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [handleResize]);
 
   return windowDimensions;
 }
