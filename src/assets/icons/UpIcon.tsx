970caf7b141fe0ede6af44eb2a3f9c465094e489
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-07 20:42:50 -0300

    chore: add icons

---
+++ src/assets/icons/UpIcon.tsx
@@ -0,0 +1,34 @@
+function UpIcon() {
+  return (
+    <svg
+      width="20"
+      height="11"
+      viewBox="0 0 20 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.5 10L10 1L1.5 10"
+        stroke="url(#paint0_linear_4284_231)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_4284_231"
+          x1="10"
+          y1="1"
+          x2="10"
+          y2="10"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5A84C0" />
+          <stop offset="1" stopColor="#594ED2" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default UpIcon;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as Logo } from "./icons/Logo";
 export { default as SearchIcon } from "./icons/SearchIcon";
+export { default as UpIcon } from "./icons/UpIcon";
+export { default as DownIcon } from "./icons/DownIcon";
