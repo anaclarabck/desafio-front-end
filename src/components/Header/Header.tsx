c31218cb175275a7b266ab5f10b679b8fc54416b
Ana Clara Kyotoku <ackyotoku@gmail.com>
2024-07-07 21:01:49 -0300

    feat: add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "@/assets";
+import * as S from "./styles";
+
+function Header() {
+  return (
+    <S.HeaderDiv>
+      <Logo />
+    </S.HeaderDiv>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import { colors } from "@/constants";
+import styled from "styled-components";
+
+const HeaderDiv = styled.div`
+  @media (min-width: 376px) {
+    padding-left: 32px;
+  }
+
+  align-items: center;
+  background-color: ${colors.white};
+  box-shadow: 0px 2px 8px 0px #00000033;
+  display: flex;
+  height: 60px;
+  padding-left: 20px;
+`;
+
+export { HeaderDiv };
